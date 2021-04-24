561424880079fa3f7853807e80a7a0d20d042dd8
Le Zhang <54585080+leleonardzhang@users.noreply.github.com>
2021-04-24 18:55:39 -0400

    apr24

--- src/main/TestFrame.java
+++ src/main/TestFrame.java
@@ -1,23 +1,15 @@
 package main;
 
-import javax.swing.JButton;
-import javax.swing.JSlider;
-
 import bus.uigen.OEFrame;
 import bus.uigen.ObjectEditor;
 import components.mainpanel.MainPanel;
 import components.mainpanel.MainPanelInterface;
-import components.pendulum.load.Load;
-import components.pendulum.load.LoadInterface;
-import components.pendulum.protractor.Protractor;
-import components.pendulum.protractor.ProtractorInterface;
-import components.pendulum.string.PendulumString;
-import components.pendulum.string.PendulumStringInterface;
-import javafx.scene.control.Slider;
 
 public class TestFrame {
 	public static void main(String[] args) {
 		MainPanelInterface panel = new MainPanel();
 		OEFrame frame = ObjectEditor.edit(panel);
+		frame.setSize(1400, 1050);
+		frame.setLocation(600, 0);
 	}
 }
